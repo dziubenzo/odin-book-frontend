1cb423a3122e62545986037951e169b0277d2a9d
dziubenzo <dziubenzo@gmail.com>
2024-05-28 13:58:55 +0200

    Improve ErrorPage component

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,14 +1,21 @@
 import { useRouteError } from 'react-router-dom';
 import { StyledErrorPage } from '../styles/ErrorPage.styled';
+import Theme from '../components/Theme';
+import GlobalStyle from '../styles/GlobalStyle';
+import { Link } from 'react-router-dom';
 
 function ErrorPage() {
   const error = useRouteError();
 
   return (
-    <StyledErrorPage>
-      <h2>{error.status || 500}</h2>
-      <h1>{error.statusText || 'Error'}</h1>
-    </StyledErrorPage>
+    <Theme>
+      <GlobalStyle />
+      <StyledErrorPage>
+        <h2>{error.status || 500}</h2>
+        <h1>{error.statusText || 'Error'}</h1>
+        <Link to={'/posts'}>Back to Main Page</Link>
+      </StyledErrorPage>
+    </Theme>
   );
 }
 
--- src/styles/ErrorPage.styled.js
+++ src/styles/ErrorPage.styled.js
@@ -3,6 +3,28 @@ import styled from 'styled-components';
 export const StyledErrorPage = styled.main`
   display: flex;
   flex-direction: column;
-  gap: 16px;
+  align-items: center;
+  justify-content: center;
+  gap: 24px;
   text-align: center;
+  min-height: 100vh;
+
+  a {
+    position: absolute;
+    bottom: 4em;
+    text-decoration: none;
+    font-size: ${(props) => props.theme.fontSizes.standard};
+  }
+
+  @media (hover: hover) {
+    a {
+      height: 22px;
+      width: 160px;
+
+      &:hover {
+        border-radius: 8px;
+        background-color: ${(props) => props.theme.colours.tertiary};
+      }
+    }
+  }
 `;
