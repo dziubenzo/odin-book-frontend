d46b72b5c9a1f6914e628a05d3a0690e802159e7
dziubenzo <dziubenzo@gmail.com>
2024-06-11 12:52:16 +0200

    Write CommentInputBottom component tests

--- src/tests/CommentInput.test.jsx
+++ src/tests/CommentInput.test.jsx
@@ -4,6 +4,7 @@ import { render, screen } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 
 import CommentInput from '../components/CommentInput';
+import CommentInputBottom from '../components/CommentInputBottom';
 import Theme from '../components/Theme';
 
 import { detailedPost1, superUser, longComment } from './mocks';
@@ -24,6 +25,24 @@ function renderComponent() {
   );
 
   return { user };
+}
+
+function renderCommentInputBottom(
+  handleSubmitCommentClick,
+  inProgress = false,
+  isSubmitted = false,
+  commentError = '',
+) {
+  render(
+    <Theme>
+      <CommentInputBottom
+        handleSubmitCommentClick={handleSubmitCommentClick}
+        inProgress={inProgress}
+        isSubmitted={isSubmitted}
+        commentError={commentError}
+      />
+    </Theme>,
+  );
 }
 
 describe('CommentInputTop', () => {
@@ -68,6 +87,18 @@ describe('CommentInputTop', () => {
     expect(commentLengthPara).toBeInTheDocument();
   });
 
+  it('should render a comment length paragraph that cannot exceed MAX_COMMENT_LENGTH', async () => {
+    const { user } = renderComponent();
+
+    const commentInputField = screen.getByTestId('comment-input-field');
+    await user.click(commentInputField);
+    await user.type(commentInputField, longComment + 'I will now be too long');
+    const commentLengthPara = screen.getByText(/0/i);
+
+    expect(commentLengthPara).toBeInTheDocument();
+    expect(commentInputField.textContent.length).toBe(MAX_COMMENT_LENGTH);
+  });
+
   it('should render a comment length paragraph that has a different colour if the comment length is smaller than MIN_COMMENT_LENGTH', async () => {
     const { user } = renderComponent();
     const typedText = 'Oi';
@@ -107,5 +138,51 @@ describe('CommentInputTop', () => {
     );
 
     expect(commentLengthPara).not.toHaveClass('warning');
+  });
+});
+
+describe('CommentInputTop', () => {
+  it('should render a Submit button', () => {
+    renderComponent();
+
+    const submitButton = screen.getByRole('button');
+
+    expect(submitButton).toBeInTheDocument();
+  });
+
+  it('should call a function to create a new comment if the Submit button is clicked', async () => {
+    const createCommentFn = vi.fn();
+    const user = userEvent.setup();
+    renderCommentInputBottom(createCommentFn);
+
+    const submitButton = screen.getByRole('button');
+    await user.click(submitButton);
+
+    expect(createCommentFn).toHaveBeenCalledTimes(1);
+  });
+
+  it("should change the description of the Submit button to 'Submitting...' if a new comment is being created", () => {
+    renderCommentInputBottom(vi.fn(), true);
+
+    const submitButton = screen.getByRole('button');
+
+    expect(submitButton.textContent).toBe('Submitting...');
+  });
+
+  it("should change the description of the Submit button to 'Submitted!' if a new comment has just been created", () => {
+    renderCommentInputBottom(vi.fn(), false, true);
+
+    const submitButton = screen.getByRole('button');
+
+    expect(submitButton.textContent).toBe('Submitted!');
+  });
+
+  it('should render an error message if the comment has not been created', () => {
+    const errorMessage = 'A massive failure!';
+    renderCommentInputBottom(vi.fn(), false, false, errorMessage);
+
+    const errorMessageParagraph = screen.getByText(errorMessage);
+
+    expect(errorMessageParagraph).toBeInTheDocument();
   });
 });
